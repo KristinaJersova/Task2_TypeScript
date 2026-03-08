daade83aa93ea60615e3a52e2c4bbe4cc0379d7a
Kristina Jeršova <kristinajershova5@gmail.com>
2026-03-08 15:49:52 +0200

    Add export function to Average Review

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -1,1 +1,14 @@
 import { Review } from "../models/Review";
+
+export function getAverageRating(productId: number, reviews: Review[]): number | null {
+
+    const productReviews = reviews.filter(r => r.productId === productId);
+
+    if (productReviews.length === 0) {
+        return null;
+    }
+
+    const sum = productReviews.reduce((acc, r) => acc + r.rating, 0);
+
+    return sum / productReviews.length;
+}
