99bf3050994d204650b12fce7fb331e112878ee4
Kristina Jeršova <kristinajershova5@gmail.com>
2026-03-09 13:00:22 +0200

    added category to task1

--- src/task1.ts
+++ src/task1.ts
@@ -1,20 +1,23 @@
-import {products} from "./data/storeData";
-
+import { products } from "./data/storeData";
+import { Category } from "./models/Category";
 
 let report = "Product Report:\n\n";
 
 products.forEach(product => {
-    report += `Name: ${product.name}\n`
-    report += `Price: ${product.price.toFixed(2)}\n`
-    report += `Quantity: ${product.quantity}\n`
-    report += `Category: ${product.category}\n`
-    report += `Supplier: ${product.Supplier.name}\n`
-    if (product.specifications){
+    report += `Name: ${product.name}\n`;
+    report += `Price: ${product.price.toFixed(2)}\n`;
+    report += `Quantity: ${product.quantity}\n`;
+    report += `Category: ${Category[product.category]}\n`;
+    report += `Supplier: ${product.Supplier.name}\n`;
+
+    if (product.specifications) {
         report += "Specifications:\n";
         for (const [key, value] of Object.entries(product.specifications)) {
-            report += `  -${key}: ${value}\n`;
+            report += `  - ${key}: ${value}\n`;
         }
     }
+
     report += "\n";
 });
-console.log(report)
+
+console.log(report);
